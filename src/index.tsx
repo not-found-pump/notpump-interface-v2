a01bfe95349683874037cf34d05e14f378659be0
athevinha <nguyenthevinh1002@gmail.com>
2024-08-11 16:22:50 +0700

    test deploy

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,6 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(<App />);
 
 // If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
 serviceWorkerRegistration.unregister();
 
