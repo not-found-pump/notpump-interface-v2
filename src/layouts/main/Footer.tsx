dc5a5570c864b26d54e2c946479d6685cd768372
athevinha <nguyenthevinh1002@gmail.com>
2024-08-11 15:11:43 +0700

    text & gh flow

--- src/layouts/main/Footer.tsx
+++ src/layouts/main/Footer.tsx
@@ -95,8 +95,7 @@ export default function Footer() {
 
           <Grid item xs={8} md={3}>
             <Typography variant="body2" sx={{ pr: { md: 5 } }}>
-              The starting point for your next project with NotPump Kit, built on the newest
-              version of Material-UI ©, ready to be customized to your style.
+            The starting point for your NFT project with NotPump
             </Typography>
 
             <Stack
